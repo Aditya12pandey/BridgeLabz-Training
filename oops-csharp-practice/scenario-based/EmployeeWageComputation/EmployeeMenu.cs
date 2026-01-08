3443f0fd9999766fb862aedcb8df0cc45931bfc9
Aditya Vikram Pandey <adityavikrammvm@gmail.com>
2026-01-08 11:13:53 +0530

    [Aditya Vikram Pandey]Add .Added Employee Wages Calculator UC1

---
+++ oops-csharp-practice/scenario-based/EmployeeWageComputation/EmployeeMenu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    sealed class EmployeeMenu
+    {
+        public void ShowMenu()
+        {
+            Console.WriteLine("\n Employee Details ");
+
+            Console.Write("Enter Employee ID   : ");
+            int empId = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Employee Name : ");
+            string empName = Console.ReadLine();
+
+            Employee employee = new Employee
+            {
+                EmpId = empId,
+                EmpName = empName
+            };
+
+            IEmployee empUtility = new EmployeeUtility(employee);
+
+            Console.WriteLine("\n MENU ");
+            Console.WriteLine("1. Check Employee Attendance (UC-1)");
+            Console.Write("Enter your choice: ");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    empUtility.CheckAttendance();
+                    break;
+
+                default:
+                    Console.WriteLine("❌ Invalid choice");
+                    break;
+            }
+        }
+    }
+}
---
+++ oops-csharp-practice/scenario-based/EmployeeWageComputation/EmployeeUtility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class EmployeeUtility: IEmployee
+    {
+        Employee employee;
+        Random random = new Random();
+
+        // Accept employee from menu
+        public EmployeeUtility(Employee emp)
+        {
+            employee = emp;
+        }
+
+        // UC-1 Logic
+        public void CheckAttendance()
+        {
+            Console.WriteLine("Employee ID   : " + employee.EmpId);
+            Console.WriteLine("Employee Name : " + employee.EmpName);
+
+            int attendance = random.Next(0, 2);
+
+            Console.WriteLine("\n Attendance Status");
+            if (attendance == 1)
+                Console.WriteLine("✅ Employee is Present");
+            else
+                Console.WriteLine("❌ Employee is Absent");
+        }
+
+    }
+}
