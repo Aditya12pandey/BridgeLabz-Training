233f9e0196943ce32a272c1462ed234794b2c903
Aditya Vikram Pandey <adityavikrammvm@gmail.com>
2026-01-17 15:59:57 +0530

    [Aditya Vikram Pandey]Add .Added Scenario based Fitness tracker

---
+++ data-structure-csharp-practice/scenario-based/FitnessTracker/IFitnessTracker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraning.Datastructure.FitnessTracker
+{
+    internal interface IFitnessTracker
+    {
+        void AddUser(string name, int steps);
+        void UpdateSteps(string name, int newSteps);
+        void DisplayAllUsers();
+        void SortByStepsDesc();
+        void DisplayLeaderboard();
+    }
+}
