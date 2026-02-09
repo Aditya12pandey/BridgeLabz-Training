5bb8d2efeeebba54e67e9769ac451d6bfb88a1cd
Aditya Vikram Pandey <adityavikrammvm@gmail.com>
2026-02-09 22:32:44 +0530

    [Aditya Vikram Pandey]Add . Added Healthcare Application

---
+++ database-csharp-practice/scenario-based/HealthCareApplication/DBConnect/StudentConnection.cs
@@ -0,0 +1,12 @@
+// using Microsoft.Data.SqlClient;
+
+// class StudentConnection
+// {
+//     public static void Main(string[] args)
+//     {
+//         string connectionString="Server=DESKTOP-EV2BVPS\\SQLEXPRESS;Database = HospitalDatabase;Trusted_Connection = True;TrustServerCertificate=True;";
+//         using SqlConnection connection = new SqlConnection(connectionString);
+//         connection.Open();
+//         System.Console.WriteLine("Connection Successful");
+//     }
+// }
